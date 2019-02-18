f6091d4e55157e991c53d23ea150ccd4938fcd73
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2019-02-18 01:36:42 -0500

    Removed knitr options from functions

--- R/case_study.R
+++ R/case_study.R
@@ -3,16 +3,10 @@ case_study <- function(...){
   # Define filepaths
   template <- system.file(package = "hmdrmd", "header.tex")
   
+  
   # supply files to your custom format
   rmarkdown::pdf_document(..., 
                           includes = rmarkdown::includes(in_header = template),
                           latex_engine = "xelatex",
                           highlight = "kate")
-  
-  
-  #Set knitr options
-  knitr::opts_chunk$set(echo = TRUE)
-  
-  knitr::opts_chunk$set(tidy.opts=list(width.cutoff=80))
-  
 }
