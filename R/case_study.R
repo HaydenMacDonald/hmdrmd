ff807740f8669f869cacac72ae5d96683d4cfb8a
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2019-02-18 02:43:32 -0500

    Changed case_study to match use of format object to edit knitr chunk options

--- R/case_study.R
+++ R/case_study.R
@@ -1,20 +1,20 @@
 case_study <- function(...){
   
   # Define filepaths
-  template <- system.file(package = "hmdrmd", "header.tex")
+  template <- system.file("header.tex", package = "hmdrmd")
   
   
   # supply files to your custom format
-  rmarkdown::pdf_document(..., 
+  format <- rmarkdown::pdf_document(..., 
                           includes = rmarkdown::includes(in_header = template),
                           latex_engine = "xelatex",
                           highlight = "kate")
   
   
   #Set output format to take knitr options
-  rmarkdown::output_format(
-    base_format = "pdf_document",
-    knitr = rmarkdown::knitr_options(opts_chunk = list(tidy.opts=list(width.cutoff=80))),
-    pandoc = rmarkdown::pandoc_options(to = "pdf")
-  )
+  chunk_options <- rmarkdown::knitr_options(opts_chunk = list(tidy.opts=list(width.cutoff=80)))
+  
+  format$knitr <- chunk_options
+  format$inherits <- 'pdf_document'
+  format
 }
