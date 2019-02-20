cd9f440cab8169cc692dc651617eadb8dad8e481
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2019-02-20 14:55:12 -0500

    Added knitr_options to functions

--- R/case_study.R
+++ R/case_study.R
@@ -25,7 +25,8 @@ case_study <- function(...){
   
   
   #Set output format to take knitr options
-  chunk_options <- rmarkdown::knitr_options(opts_chunk = list(tidy.opts=list(width.cutoff=80), background = "#F5F5F5"))
+  chunk_options <- rmarkdown::knitr_options(opts_chunk = list(tidy.opts=list(width.cutoff=80)))
+  knitr_options$opts_chunk$background = "#F5F5F5"
   
   format$knitr <- chunk_options
   format$inherits <- 'pdf_document'
